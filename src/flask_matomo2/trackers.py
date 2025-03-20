e3a3bdff60418ed7985cf7e554cc4a1ac651efd2
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-03-20 11:21:51 +0100

    test: add doc-test for trackers

--- src/flask_matomo2/trackers.py
+++ src/flask_matomo2/trackers.py
@@ -1,4 +1,10 @@
-"""Trackers for time measurements."""
+"""Trackers for time measurements.
+
+>>> scope = {"tracking_data": {}}
+>>> with PerfMsTracker(scope, key="pf_srv"):
+...     _a = 2 + 2 # do computation
+>>> assert "pf_srv" in scope["tracking_data"]
+"""
 
 from matomo_core.trackers import PerfMsTracker
 
